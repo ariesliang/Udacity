c2a493089b3e6ac392b253ab821a54225f2dd8b5
ariesliang <aries1220yuan@gmail.com>
2018-05-28 14:10:12 -0400

    Puzzler initial full game commit

---
+++ Puzzler/Assets/PuzzlerVR/Scripts/Door.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine; 
+
+public class Door : MonoBehaviour {
+
+    public GameObject player;
+    private Animator animator;
+
+	// Use this for initialization
+	void Start () {
+        animator = GetComponent<Animator>();
+	} 
+
+    public void OpenDoor()
+    {
+        animator.SetBool("player_nearby", true);
+        this.GetComponent<AudioSource>().Play();  
+    }
+
+    public void CloseDoor()
+    {
+        animator.SetBool("player_nearby", false);
+    }
+}
---
+++ Puzzler/Assets/PuzzlerVR/Scripts/OrbController.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class OrbController : MonoBehaviour
+{
+    public GameObject s1;
+    public GameObject o;
+    public GameObject s2;
+
+    private void Start()
+    {
+        iTween.MoveTo(s1, iTween.Hash("path", iTweenPath.GetPath("s_shape_1"), 
+                                            "time", 10f, 
+                                            "loopType", "pingPong", 
+                                            "easeType", iTween.EaseType.easeInOutSine));
+        iTween.MoveTo(o, iTween.Hash("path", iTweenPath.GetPath("o_shape"),
+                                            "time", 10f,
+                                            "loopType", "pingPong",
+                                            "easeType", iTween.EaseType.easeInOutSine));
+        iTween.MoveTo(s2, iTween.Hash("path", iTweenPath.GetPath("s_shape_2"),
+                                           "time", 10f,
+                                           "loopType", "pingPong",
+                                           "easeType", iTween.EaseType.easeInOutSine));
+    }
+}
+
+
